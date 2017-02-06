e54d6be7b9bf2a8baccca22742b5209456a4c887
dell-pc <dell-pc@193.1.165.93>
2017-02-06 13:54:17 +0000

    abc

---
+++ systeminfo/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name="systeminfo",
+      version="0.1",
+      description="Basic system information for COMP30670",
+      url="",
+      author="aonghus lawlor",
+      author_email="aonghus.lawlor@ucd.ie",
+      licence="GPL3",
+      PACAGES=['systeminfo'],
+      entry_points={
+          'console_scripts':['com30670_systeminfo=systeminfo.main:main']})
+f'f'f'f'f
