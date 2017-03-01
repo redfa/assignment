ea124292cddf8882d4e1311c1f61b99f79385da6
dell-pc <dell-pc@193.1.165.18>
2017-03-01 16:37:42 +0000

    df

---
+++ assignment3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name="assignment3",
+      version="0.1",
+      description="LED Testing for Assignment3 in COMP30670 2017",
+      url="",
+      author="Peng Xu",
+      author_email="aonghus.lawlor@ucd.ie",
+      licence="GPL3",
+      packages=['NAME'],
+      entry_points={
+        'console_scripts':['led_tester=NAME.main:main']
+        },
+      install_requires=[
+          'numpy',
+      ],
+      )
